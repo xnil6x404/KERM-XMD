28fa897dbf9412b90d923d0189335f7d63c963bc
•𝐋𝐎𝐑𝐃 𝐊𝐄𝐑𝐌• <kermd237@gmail.com>
2025-03-24 13:50:42 +0100

    Update stickersearch.js

--- plugins/stickersearch.js
+++ plugins/stickersearch.js
@@ -1,45 +1,50 @@
 const {
-    smd,
-    tlang,
-    prefix,
-    Config,
-    sleep,
-    astroJson,
-    smdBuffer
-  } = require("../lib");
-  const axios = require("axios");
-  smd({
-    cmdname: "stickersearch",
-    alias: ["sticsearch"],
-    category: "search",
-    use: "[text]",
-    info: "Searches Stickers"
-  }, async (_0x4cc234, _0xa151c7) => {
-    try {
-      const {
-        generateSticker: _0x5a889c
-      } = require("../lib");
-      if (!_0xa151c7) {
-        return _0x4cc234.reply("Sorry you did not give any search term!");
-      }
-      const _0x1f4c84 = await axios.get("https://g.tenor.com/v1/search?q=" + _0xa151c7 + "&key=LIVDSRZULELA&limit=8").catch(() => {});
-      if (!_0x1f4c84.data || !_0x1f4c84.data.results || !_0x1f4c84.data.results[0]) {
-        return _0x4cc234.reply("*Could not find!*");
-      }
-      let _0x43fcbc = _0x1f4c84.data.results.length > 5 ? 5 : _0x1f4c84.data.results.length;
-      for (let _0x48da09 = 0; _0x48da09 < _0x43fcbc; _0x48da09++) {
-        let _0x2a4632 = await smdBuffer(_0x1f4c84.data.results?.[_0x48da09]?.media[0]?.mp4?.url);
-        let _0x1c1454 = {
-          pack: Config.packname,
-          author: Config.author,
-          type: "full",
-          quality: 1
-        };
-        if (_0x2a4632) {
-          _0x5a889c(_0x4cc234, _0x2a4632, _0x1c1454);
-        }
-      }
-    } catch (_0x3f900e) {
-      _0x4cc234.error(_0x3f900e + "\n\nCommand: stickersearch", _0x3f900e, "*Could not find*");
+  smd,
+  tlang,
+  prefix,
+  Config,
+  sleep,
+  astroJson,
+  smdBuffer
+} = require("../lib");
+
+const axios = require("axios");
+
+smd({
+  cmdname: "stickersearch",
+  alias: ["sticsearch"],
+  category: "search",
+  use: "[text]",
+  info: "Searches Stickers"
+}, async (message, searchTerm) => {
+  try {
+    const { generateSticker } = require("../lib");
+    if (!searchTerm) {
+      return message.reply("Sorry you did not give any search term!");
     }
-  });
+
+    const response = await axios.get(`https://g.tenor.com/v1/search?q=${searchTerm}&key=LIVDSRZULELA&limit=8`).catch(() => {});
+    if (!response.data || !response.data.results || !response.data.results[0]) {
+      return message.reply("*Could not find!*");
+    }
+
+    const resultsLimit = response.data.results.length > 5 ? 5 : response.data.results.length;
+    for (let i = 0; i < resultsLimit; i++) {
+      const mediaUrl = response.data.results[i]?.media[0]?.mp4?.url;
+      const stickerBuffer = await smdBuffer(mediaUrl);
+      
+      const stickerOptions = {
+        pack: Config.packname,
+        author: Config.author,
+        type: "full",
+        quality: 1
+      };
+
+      if (stickerBuffer) {
+        generateSticker(message, stickerBuffer, stickerOptions);
+      }
+    }
+  } catch (error) {
+    message.error(error + "\n\nCommand: stickersearch", error, "*Could not find*");
+  }
+});
